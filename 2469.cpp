95b42fdd7ad4c1917da66ce7b2dc422080fec41d
Brandon <baej12@unlv.nevada.edu>
2024-09-05 23:00:57 -0700

    daily quesiton

---
+++ 2469.cpp
@@ -0,0 +1,12 @@
+#include <./stdc++11.h>
+
+using namespace std;
+
+vector<double> convertTemperature(double celsius) {
+    return vector<double>{celsius + 273.15, celsius * 1.8 + 32.0};
+}
+
+int main () {
+
+    return 0;
+}
